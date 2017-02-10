cc1328997e2dfc9e7cdd01661f33f3b88bf45d00
digitalartcrew <digitalartcrew@gmail.com>
2017-02-10 14:30:46 -0800

    Update

--- public/js/app.js
+++ public/js/app.js
@@ -6,84 +6,108 @@ app.config(function($stateProvider,$urlRouterProvider) {
     url: '/login',
     templateUrl: '/views/login.html',
     controller: 'LoginCtrl',
-    authorization: false
+    resolve: {
+      //..
+    }
   })
 
   .state('signup', {
     url: '/signup',
     templateUrl: 'views/signup.html',
     controller: 'SignUpCtrl',
-    authorization: false
+    resolve: {
+      //..
+    }
   })
   
   .state('form', {
     url: '/form',
     templateUrl: 'views/form.html',
     controller: 'FormCtrl',
-    authorization: true
+    resolve: {
+      //..
+    }
   })
 
   .state('form.main', {
     url: '/',
     templateUrl: 'views/main.html',
     controller: 'MainCtrl',
-    authorization: true
+    resolve: {
+      //..
+    }
   })
 
   .state('form.account', {
     url: '/account',
     templateUrl: 'views/account.html',
     controller: 'AccountCtrl',
-    authorization: true
+    resolve: {
+      //..
+    }
   })
 
   .state('form.artists', {
     url: '/artists',
     templateUrl: 'views/artists.html',
     controller: 'ArtistsCtrl',
-    authorization: true
+    resolve: {
+      //..
+    }
   })
 
   .state('form.artist-profile', {
     url: '/artists/:id',
     templateUrl: 'views/artist-profile.html',
     controller: 'ArtistProfileCtrl',
-    authorization: true
+    resolve: {
+      //..
+    }
   })
 
   .state('form.teams', {
     url: '/teams',
     templateUrl: 'views/teams.html',
     controller: 'TeamsCtrl',
-    authorization: true
+    resolve: {
+      //..
+    }
   })
 
   .state('form.team-profile', {
     url: '/teams:id',
     templateUrl: 'views/team-profile.html',
     controller: 'TeamProfileCtrl',
-    authorization: true
+    resolve: {
+      //..
+    }
   })
 
   .state('form.events', {
     url: '/events',
     templateUrl: 'views/events.html',
     controller: 'EventCtrl',
-    authorization: true
+    resolve: {
+      //..
+    }
   })
 
   .state('form.projects', {
     url: '/projects',
     templateUrl: 'views/projects.html',
     controller: 'ProjectsCtrl',
-    authorization: true
+    resolve: {
+      //..
+    }
   })
 
   .state('form.contest', {
     url: '/contests',
     templateUrl: 'views/contest.html',
     controller: 'ContestCtrl',
-    authorization: true
+    resolve: {
+      //..
+    }
   })
   
   $urlRouterProvider.otherwise('login');
