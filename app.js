7c15389bf4fb40357b55e68ca3eb4f58a87bbf20
digitalartcrew <digitalartcrew@gmail.com>
2017-02-10 23:47:00 -0800

    Updated app.sj

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ methodOverride = require('method-override'),
 morgan = require("morgan"),
 path = require("path"),
 session = require('express-session'),
-// MongoDBStore = require('connect-mongodb-session')(session);
 
 app.use(express.static(__dirname + '/public'));
 
@@ -14,6 +13,12 @@ var passport = require('passport');
 require('./config/passport')(passport); //passport configuration
 
 
+app.use(session({
+  secret: 'awesome',
+  resave: true,
+    saveUninitialized: true
+}));
+
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -21,7 +26,7 @@ app.use(passport.session());
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-	extended: true
+  extended: true
 }));
 
 // use morgan
@@ -33,5 +38,5 @@ app.use(methodOverride('_method'));
 //Routes
 require('./routes/auth.js')(app,passport); //load our routes and full configured passport
 app.listen(process.env.PORT || 3000, function(req,res){
-	console.log("App running on localost 3000");
+  console.log("App running on localost 3000");
 });
