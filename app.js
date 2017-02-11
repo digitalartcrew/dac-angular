a0dc685f5334f3009078c8bfc2b6eb3b888ec126
digitalartcrew <digitalartcrew@gmail.com>
2017-02-10 18:29:24 -0800

    Update app.js

--- app.js
+++ app.js
@@ -14,32 +14,6 @@ var passport = require('passport');
 require('./config/passport')(passport); //passport configuration
 
 
-var store = new MongoDBStore(
-{
-	uri: process.env.MONGOLAB_URI || 'mongodb://localhost:27017/connect_mongodb_session_test',
-	collection: 'mySessions'
-});
-
-    // Catch errors 
-    store.on('error', function(error) {
-    	assert.ifError(error);
-    	assert.ok(false);
-    });
-
-    app.use(require('express-session')({
-    	secret: 'This is a secret',
-    	cookie: {
-        maxAge: 1000 * 60 * 60 * 24 * 7 // 1 week 
-    },
-    store: store,
-      // Boilerplate options, see: 
-      // * https://www.npmjs.com/package/express-session#resave 
-      // * https://www.npmjs.com/package/express-session#saveuninitialized 
-      resave: true,
-      saveUninitialized: true
-  }));
-
-
 app.use(passport.initialize());
 app.use(passport.session());
 
