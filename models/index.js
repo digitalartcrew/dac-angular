be8bb38ee727cdf4be5e3ee3e2777cc7afbbe3bc
digitalartcrew <digitalartcrew@gmail.com>
2017-02-11 16:00:36 -0800

    Update

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 var mongoose = require("mongoose");
-mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/dac-angular');
+
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://heroku_014b7mf2:8llht4po9su63o52v659e669g3@ds139899.mlab.com:39899/heroku_014b7mf2');
 
 mongoose.set("debug", true);
 
